b3055e47434478d79c15ea1f86ceac69f5d665d5
Роман Подгорный <110032141+ripodgor@users.noreply.github.com>
2022-09-28 12:20:20 +0300

    create files

---
+++ src/main/java/ru/netology/StatsService.java
@@ -0,0 +1,86 @@
+package ru.netology;
+
+public class StatsService {
+    public int getSum(int[] sales) {
+        int sum = 0;
+        for (int sale : sales) {
+            sum += sale;
+        }
+        return sum;
+    }
+
+    public int getMiddleSum(int[] sales) {
+        return getSum(sales) / sales.length;
+    }
+
+    public int getMinMonth(int[] sales) {
+        int min = Min(sales);
+        int monthCount = 0;
+        int minMonth = 0;
+        for (int sale : sales) {
+            monthCount++;
+            if (sale == min) {
+                minMonth = monthCount;
+            }
+        }
+        return minMonth;
+    }
+
+    private int Min(int[] sales) {
+        int min = sales[0];
+        for (int sale : sales) {
+            if (sale < min) {
+                min = sale;
+            }
+        }
+        return min;
+    }
+
+
+    public int getMaxMonth(int[] sales) {
+        int max = Max(sales);
+        int monthCount = 0;
+        int monthWithMax = 0;
+        for (int sale : sales) {
+            monthCount++;
+            if (sale == max) {
+                monthWithMax = monthCount;
+            }
+        }
+        return monthWithMax;
+    }
+
+    private int Max(int[] sales) {
+        int max = sales[0];
+        for (int sale : sales) {
+            if (sale > max) {
+                max = sale;
+            }
+        }
+        return max;
+    }
+
+    public int getLowestMonths(int[] sales) {
+        int middle = getMiddleSum(sales);
+
+        int lowestMonths = 0;
+        for (int sale : sales) {
+            if (sale < middle) {
+                lowestMonths++;
+            }
+        }
+        return lowestMonths;
+    }
+
+    public int getBestMonths(int[] sales) {
+        int middle = getMiddleSum(sales);
+
+        int bestMonths = 0;
+        for (int sale : sales) {
+            if (sale > middle) {
+                bestMonths++;
+            }
+        }
+        return bestMonths;
+    }
+}
---
+++ src/test/java/ru/netology/StatsServiceTest.java
@@ -0,0 +1,40 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatsServiceTest {
+    private StatsService service = new StatsService();
+    private int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+    @Test
+    void ShouldCalculateSum() {
+        assertEquals(180, service.getSum(sales));
+    }
+
+    @Test
+    void ShouldCalculateMiddleSum() {
+        assertEquals(15, service.getMiddleSum(sales));
+    }
+
+    @Test
+    void ShouldCalculateMinMonth() {
+        assertEquals(9, service.getMinMonth(sales));
+    }
+
+    @Test
+    void ShouldCalculateMaxMonth() {
+        assertEquals(8, service.getMaxMonth(sales));
+    }
+
+    @Test
+    void ShouldCalculateLowestMonths() {
+        assertEquals(5, service.getLowestMonths(sales));
+    }
+
+    @Test
+    void ShouldCalculateBestMonths() {
+        assertEquals(5, service.getBestMonths(sales));
+    }
+}
